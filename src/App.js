6cde950c45571b1272190cf27d62c7a7ceebf193
igorkishko <kishko.igor@gmail.com>
2023-08-04 16:01:00 +0300

    add mobile layout

--- src/App.js
+++ src/App.js
@@ -25,9 +25,8 @@ const App = () => {
           <header className='header'>
             <h1 className='heading'>Wallet balance</h1>
             <p className='description'>
-              Multisig wallet is a wallet for all company founds operations,{' '}
-              <br />
-              see how it works on airdaowiki
+              Multisig wallet is a wallet for all company founds operations, see
+              how it works on airdaowiki
             </p>
           </header>
 
@@ -54,61 +53,61 @@ const App = () => {
                 </button>
               </div>
             </div>
-
-            <div className='wallet-balances'>
-              {tableData.map((wallet) => (
-                <div
-                  key={wallet.multisigName}
-                  className={`wallet-balance-card`}
-                >
-                  <h4 className='wallet-name'>
-                    {wallet.multisigName.replace('Finance', '')}
-                  </h4>
-                  <span className='wallet-balance'>
-                    {(+wallet.balance)
-                      .toFixed(2)
-                      .replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1,')}
-                  </span>
-                </div>
-              ))}
-            </div>
-
-            <div className='table'>
-              {Array(maxTxsLength)
-                .fill(0)
-                .map((_, i) => (
-                  <div className='table__row' key={i}>
-                    <div className='table__cell table__cell_info'>
-                      <div className='cell-title'>Tx hash</div>
-                      <div className='cell-title'>Amount</div>
+            <div className='scroll-container'>
+              <div className='table'>
+                <div className='wallet-balances'>
+                  {tableData.map((wallet) => (
+                    <div
+                      key={wallet.multisigName}
+                      className={`wallet-balance-card`}
+                    >
+                      <h4 className='wallet-name'>
+                        {wallet.multisigName.replace('Finance', '')}
+                      </h4>
+                      <span className='wallet-balance'>
+                        {(+wallet.balance)
+                          .toFixed(2)
+                          .replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1,')}
+                      </span>
                     </div>
-                    {tableData.map((el) => (
-                      <div key={el.multisigName} className='table__cell'>
-                        {el.txs[i] && (
-                          <>
-                            <a
-                              href={`https://airdao.io/explorer/tx/${el.txs[i].transactionHash}`}
-                              target='_blank'
-                              rel='noreferrer'
-                              className='tx-link'
-                            >
-                              {formatString(el.txs[i].transactionHash)}
-                            </a>
-                            <p className='tx-amount'>
-                              {utils
-                                .formatEther(el.txs[i].args.amount)
-                                .replace(
-                                  /(\d)(?=(\d{3})+([^\d]|$))/g,
-                                  '$1,'
-                                )}{' '}
-                              AMB
-                            </p>
-                          </>
-                        )}
+                  ))}
+                </div>
+                {Array(maxTxsLength)
+                  .fill(0)
+                  .map((_, i) => (
+                    <div className='table__row' key={i}>
+                      <div className='table__cell table__cell_info'>
+                        <div className='cell-title'>Tx hash</div>
+                        <div className='cell-title'>Amount</div>
                       </div>
-                    ))}
-                  </div>
-                ))}
+                      {tableData.map((el) => (
+                        <div key={el.multisigName} className='table__cell'>
+                          {el.txs[i] && (
+                            <>
+                              <a
+                                href={`https://airdao.io/explorer/tx/${el.txs[i].transactionHash}`}
+                                target='_blank'
+                                rel='noreferrer'
+                                className='tx-link'
+                              >
+                                {formatString(el.txs[i].transactionHash)}
+                              </a>
+                              <p className='tx-amount'>
+                                {utils
+                                  .formatEther(el.txs[i].args.amount)
+                                  .replace(
+                                    /(\d)(?=(\d{3})+([^\d]|$))/g,
+                                    '$1,'
+                                  )}{' '}
+                                AMB
+                              </p>
+                            </>
+                          )}
+                        </div>
+                      ))}
+                    </div>
+                  ))}
+              </div>
             </div>
           </section>
         </main>
